d2a21e3c3848dc023afd13572680885271a4e08c
mseduarda <meduardaoliv000@gmail.com>
2024-07-21 21:51:48 -0300

    Calculator: Starting of the project.

---
+++ Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Reflection;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static void Menu() {
+            Console.Clear();
+            Console.WriteLine("╔═════════════╗");
+            Console.WriteLine("  CALCULATOR  ");
+            Console.WriteLine("╚═════════════╝");
+            Console.WriteLine("┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉");
+            Console.WriteLine("Which one of the options do you want to type?");
+            Console.WriteLine("[1] - Sum");
+            Console.WriteLine("[2] - Subtract");
+            Console.WriteLine("[3] - Divide");
+            Console.WriteLine("[4] - Multiply");
+            Console.WriteLine("[5] - Bhaskara");
+            Console.WriteLine("[6] - Exit the program");
+            Console.WriteLine("┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉");
+
+            Console.WriteLine("Type the option: ");
+            short op = short.Parse(Console.ReadLine());
+
+            switch(op) {
+                case 1: Sum(); break;
+                case 2: Subtract(); break;
+                case 3: Divide(); break;
+                case 4: Multiply(); break;
+                case 5: Bhaskara(); break;
+                case 6: System.Environment.Exit(0); break;
+                default: Menu(); break;
+            }
+        }
+        static void Sum() {
+            Console.Clear();
+
+            Console.WriteLine("Type the first value: ");
+            double v1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Type the second value: ");
+            double v2 = double.Parse(Console.ReadLine());
+
+            var sum = v1 + v2;
+
+            Console.WriteLine("Sum result: " + sum);
+            Console.ReadKey();
+            Menu();
+        }
+        static void Subtract() {
+            Console.Clear();
+
+            Console.WriteLine("Type the first value: ");
+            double v1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Type the second value: ");
+            double v2 = double.Parse(Console.ReadLine());
+
+            var sub = v1 - v2;
+
+            Console.WriteLine("Subtract result: " + sub);
+            Console.ReadKey();
+            Menu();
+        }
+        static void Divide() {
+            Console.Clear();
+
+            Console.WriteLine("Type the first value: ");
+            double v1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Type the second value: ");
+            double v2 = double.Parse(Console.ReadLine());
+
+            var div = v1 / v2;
+
+            Console.WriteLine("Divide result: " + div);
+            Console.ReadKey();
+            Menu();
+        }
+        static void Multiply() {
+            Console.Clear();
+
+            Console.WriteLine("Type the first value: ");
+            double v1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Type the second value: ");
+            double v2 = double.Parse(Console.ReadLine());
+
+            var mul = v1 * v2;
+
+            Console.WriteLine("Multiply result: " + mul);
+            Console.ReadKey();
+            Menu();
+        }
+        static void Bhaskara() {
+            Console.Clear();
+
+            Console.WriteLine("Based on the second degree equation: ax^2 + bx + c = 0");
+            Console.WriteLine("Type the value of a: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Type the value of b: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Type the value of c: ");
+            double c = double.Parse(Console.ReadLine());
+
+            double delta = Math.Pow(b, 2) - (4 * a * c);
+
+            double x1 = - b + Math.Sqrt(delta) / 2 * a;
+            double x2 = - b - Math.Sqrt(delta) / 2 * a;
+
+            Console.WriteLine("Delta result: " + delta);
+            Console.WriteLine("X1 result: " + x1);
+            Console.WriteLine("X2 result: " + x2);
+            Console.ReadKey();
+            Menu();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+# **✨ Calculator**
+
+## **🚨 About**
+
+### Calculator is a console program that perfoms operands such as: Sum, Subtraction, Divide, Multiply and Bhaskara's formula. More operands such as Factorial, Percentage and Logarithm are being added. <br> It was used C# to the estructure of the logic and algorithm.
+
+## **📋 Prerequisities**
+
+### [.NET SDK 8.0.303](https://download.visualstudio.microsoft.com/download/pr/d1adccfa-62de-4306-9410-178eafb4eeeb/48e3746867707de33ef01036f6afc2c6/dotnet-sdk-8.0.303-win-x64.exe)
+### [Visual Studio Community 2022](https://visualstudio.microsoft.com/pt-br/thank-you-downloading-visual-studio/?sku=Community&channel=Release&version=VS2022&source=VSLandingPage&cid=2030&passive=false)
+### [Git for Windows](https://github.com/git-for-windows/git/releases/download/v2.45.2.windows.1/Git-2.45.2-64-bit.exe)
+
+## **🔄️ Run the program**
+
+```bash
+    # Clone the project
+    $ git clone https://github.com/meduardaoliveira/Calculator.git
+```
+```bash
+    # Enter directory
+    $ cd Calculator
+
+    # Run the project
+    $ dotnet run
+```
